78d547648cf2fcdc4ce7dd9d7a33a70c6dfa7fb2
tdleiyao <tdleiyao@gmail.com>
2022-11-01 00:15:10 +0800

    Init

---
+++ README.md
@@ -0,0 +1,37 @@
+# Port TinyMaix to APM32F072
+
+## Chip
+
+| Item         | Parameter      |
+| ------------ | -------------- |
+| Chip         | AT32F072       |
+| Arch         | ARM Cortex M0+ |
+| Freq         | 48M            |
+| Flash        | 128KB          |
+| RAM          | 16KB           |
+
+## Board 
+
+null
+
+## Development Environment
+
+MDK AC6.13.1
+
+## Step/Project
+
+Just simple edit `tm_port.h`
+
+https://github.com/tdleiyao/TinyMaix-APM32F072
+
+## Result
+
+| config | mnist | Note |
+| ------ | ----- | ---- |
+| O0 CPU | 102   |      |
+
+
+## Author
+
+[tdleiyao (雷耀)](https://github.com/tdleiyao)
+
---
+++ Source/main.c
@@ -0,0 +1,337 @@
+/*!
+ * @file        main.c
+ *
+ * @brief       Main program body
+ *
+ * @version     V1.0.1
+ *
+ * @date        2021-07-01
+ *
+ * @attention
+ *
+ *  Copyright (C) 2020-2022 Geehy Semiconductor
+ *
+ *  You may not use this file except in compliance with the
+ *  GEEHY COPYRIGHT NOTICE (GEEHY SOFTWARE PACKAGE LICENSE).
+ *
+ *  The program is only for reference, which is distributed in the hope
+ *  that it will be usefull and instructional for customers to develop
+ *  their software. Unless required by applicable law or agreed to in
+ *  writing, the program is distributed on an "AS IS" BASIS, WITHOUT
+ *  ANY WARRANTY OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the GEEHY SOFTWARE PACKAGE LICENSE for the governing permissions
+ *  and limitations under the License.
+ */
+
+#include "main.h"
+#include "stdio.h"
+#include "apm32f0xx_gpio.h"
+#include "apm32f0xx_usart.h"
+#include "apm32f0xx_rcm.h"
+//#include "apm32f0xx_misc.h"
+//#include "apm32f0xx_eint.h"
+
+#define DEBUG_USART  USART1
+
+void UsartInit(void);
+void SystemClock_HSI_Init(void);
+
+uint32_t sysTick = 0;
+
+
+//TinyMaix BEGIN
+#include "tinymaix.h"
+#if TM_MDL_TYPE == TM_MDL_INT8
+#include "mnist_valid_q.h"
+#elif TM_MDL_TYPE == TM_MDL_FP32
+#include "mnist_valid_f.h"
+#elif TM_MDL_TYPE == TM_MDL_FP16
+#include "mnist_valid_fp16.h"
+#elif TM_MDL_TYPE == TM_MDL_FP8_143
+#include "mnist_fp8_143.h"
+#elif TM_MDL_TYPE == TM_MDL_FP8_152
+#include "mnist_fp8_152.h"
+#endif
+
+#if 1
+uint8_t mnist_pic[28*28]={
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,116,125,171,255,255,150, 93,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,169,253,253,253,253,253,253,218, 30,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,169,253,253,253,213,142,176,253,253,122,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0, 52,250,253,210, 32, 12,  0,  6,206,253,140,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0, 77,251,210, 25,  0,  0,  0,122,248,253, 65,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0, 31, 18,  0,  0,  0,  0,209,253,253, 65,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,117,247,253,198, 10,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 76,247,253,231, 63,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,128,253,253,144,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,176,246,253,159, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 25,234,253,233, 35,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,198,253,253,141,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0, 78,248,253,189, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0, 19,200,253,253,141,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,134,253,253,173, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,248,253,253, 25,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,248,253,253, 43, 20, 20, 20, 20,  5,  0,  5, 20, 20, 37,150,150,150,147, 10,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,248,253,253,253,253,253,253,253,168,143,166,253,253,253,253,253,253,253,123,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,174,253,253,253,253,253,253,253,253,253,253,253,249,247,247,169,117,117, 57,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,118,123,123,123,166,253,253,253,155,123,123, 41,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+};
+#else
+uint8_t mnist_pic[28*28]={
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 36, 56,137,201,199, 95, 37,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 45,152,234,254,254,254,254,254,250,211,151,  6,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0, 46,153,240,254,254,227,166,133,251,200,254,229,225,104,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,153,234,254,254,187,142,  8,  0,  0,191, 40,198,246,223,253, 21,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  8,126,253,254,233,128, 11,  0,  0,  0,  0,210, 43, 70,254,254,254, 21,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0, 72,243,254,228, 54,  0,  0,  0,  0,  3, 32,116,225,242,254,255,162,  5,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0, 75,240,254,223,109,138,178,178,169,210,251,231,254,254,254,232, 38,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  9,175,244,253,255,254,254,251,254,254,254,254,254,252,171, 25,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0, 16,136,195,176,146,153,200,254,254,254,254,150, 16,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,162,254,254,241, 99,  3,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,118,250,254,254, 90,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,100,242,254,254,211,  7,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 54,241,254,254,242, 59,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,131,254,254,244, 64,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 13,249,254,254,152,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0, 12,228,254,254,208,  8,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0, 78,255,254,254, 66,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,209,254,254,137,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,227,255,233, 25,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,113,255,108,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+};
+#endif
+
+#define CLASS_NUM (10)
+char* labels[CLASS_NUM] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+static tm_err_t layer_cb(tm_mdl_t* mdl, tml_head_t* lh)
+{   //dump middle result
+    int h = lh->out_dims[1];
+    int w = lh->out_dims[2];
+    int ch= lh->out_dims[3];
+    mtype_t* output = TML_GET_OUTPUT(mdl, lh);
+    return TM_OK;
+    TM_PRINTF("Layer %d callback ========\n", mdl->layer_i);
+    #if 1
+    for(int y=0; y<h; y++){
+        TM_PRINTF("[");
+        for(int x=0; x<w; x++){
+            TM_PRINTF("[");
+            for(int c=0; c<ch; c++){
+            #if TM_MDL_TYPE == TM_MDL_FP32
+                TM_PRINTF("%.3f,", output[(y*w+x)*ch+c]);
+            #else
+                TM_PRINTF("%.3f,", TML_DEQUANT(lh,output[(y*w+x)*ch+c]));
+            #endif
+            }
+            TM_PRINTF("],");
+        }
+        TM_PRINTF("],\n");
+    }
+    TM_PRINTF("\n");
+    #endif
+    return TM_OK;
+}
+
+static void parse_output(tm_mat_t* outs)
+{
+    tm_mat_t out = outs[0];
+    float* data  = out.dataf;
+    float maxp = 0;
+    int maxi = -1;
+    for(int i=0; i<10; i++){
+        printf("%d: %.3f\n", i, data[i]);
+        if(data[i] > maxp) {
+            maxi = i;
+            maxp = data[i];
+        }
+    }
+    TM_PRINTF("### Predict output is: Number %d, prob %.3f\n", maxi, maxp);
+    return;
+}
+
+static uint8_t mdl_buf[MDL_BUF_LEN];
+
+//TinyMaix END
+
+
+/*!
+ * @brief       Main program
+ *
+ * @param       None
+ *
+ * @retval      None
+ *
+ * @note
+ */
+int main(void)
+{
+	UsartInit();
+	SystemClock_HSI_Init();
+	SysTick_Config(8000);
+	printf("sysclk:%d\n",RCM_ReadSYSCLKFreq());
+	
+	//TinyMaix BEGIN
+		TM_DBGT_INIT();
+    TM_PRINTF("mnist demo\n");
+    tm_mdl_t mdl;
+
+    for(int i=0; i<28*28; i++){
+        TM_PRINTF("%3d,", mnist_pic[i]);
+        if(i%28==27)TM_PRINTF("\n");
+    }
+
+    tm_mat_t in_uint8 = {3,28,28,1, {(mtype_t*)mnist_pic}};
+    tm_mat_t in = {3,28,28,1, {NULL}};
+    tm_mat_t outs[1];
+    tm_err_t res;
+    tm_stat((tm_mdlbin_t*)mdl_data); 
+
+    res = tm_load(&mdl, mdl_data, mdl_buf, layer_cb, &in);
+    if(res != TM_OK) {
+        TM_PRINTF("tm model load err %d\n", res);
+        return -1;
+    }
+
+#if (TM_MDL_TYPE == TM_MDL_INT8) || (TM_MDL_TYPE == TM_MDL_INT16) 
+    res = tm_preprocess(&mdl, TMPP_UINT2INT, &in_uint8, &in); 
+#else
+    res = tm_preprocess(&mdl, TMPP_UINT2FP01, &in_uint8, &in); 
+#endif
+    TM_DBGT_START();
+    res = tm_run(&mdl, &in, outs);
+    TM_DBGT("tm_run");
+    if(res==TM_OK) parse_output(outs);  
+    else TM_PRINTF("tm run error: %d\n", res);
+    tm_unload(&mdl); 
+		
+	//TinyMaix END
+
+	
+    while(1)
+    {
+    }
+}
+
+ /*!
+ * @brief       Redirect C Library function printf to serial port.
+ *              After Redirection, you can use printf function.
+ *
+ * @param       ch:  The characters that need to be send.
+ *
+ * @param       *f:  pointer to a FILE that can recording all information
+ *              needed to control a stream
+ *
+ * @retval      The characters that need to be send.
+ *
+ * @note
+ */
+int fputc(int ch, FILE *f)
+{
+        /** send a byte of data to the serial port */
+        USART_TxData(DEBUG_USART,(uint8_t)ch);
+
+        /** wait for the data to be send  */
+        while (USART_ReadStatusFlag(DEBUG_USART, USART_FLAG_TXBE) == RESET);
+
+        return (ch);
+}
+
+/*!
+ * @brief       HSI as System Clock
+ *
+ * @param       None
+ *
+ * @retval      None
+ *
+ * @note
+ */
+void SystemClock_HSI_Init()
+{
+    /** set HSI trimming value */
+    RCM_SetHSITrim(16);
+
+    /** Enable HSI */
+    RCM_EnableHSI();
+
+    /** select HSI as SYSCLK */
+    RCM_ConfigSYSCLK(RCM_SYSCLK_SEL_HSI);
+
+    /** reInitiatate the usart*/
+    printf("sysSource = %d", RCM_ReadSYSCLKSource());
+    printf(" HSI = %d\r\n", RCM_ReadSYSCLKFreq());
+}
+
+void UsartInit()
+{
+    GPIO_Config_T gpioConfig;
+    USART_Config_T usartConfigStruct;
+    /** Enable GPIO clock */
+    RCM_EnableAHBPeriphClock(RCM_AHB_PERIPH_GPIOA);
+
+    /** Enable Usart1 clock */
+    RCM_EnableAPB2PeriphClock(RCM_APB2_PERIPH_USART1);
+    
+    /** Connect PXx to USARTx_Tx */
+    GPIO_ConfigPinAF(GPIOA,GPIO_PIN_SOURCE_9,GPIO_AF_PIN1);
+
+    /** Connect PXx to USARTx_Rx */
+    GPIO_ConfigPinAF(GPIOA,GPIO_PIN_SOURCE_10,GPIO_AF_PIN1);
+
+    /** Configure USART Tx as alternate function push-pull */
+    gpioConfig.mode = GPIO_MODE_AF;
+    gpioConfig.pin = GPIO_PIN_9;
+    gpioConfig.speed = GPIO_SPEED_50MHz;
+    gpioConfig.outtype = GPIO_OUT_TYPE_PP;
+    gpioConfig.pupd = GPIO_PUPD_PU;
+    GPIO_Config(GPIOA, &gpioConfig);
+
+    /** Configure USART Rx as input floating */
+    gpioConfig.pin = GPIO_PIN_10;
+    GPIO_Config(GPIOA, &gpioConfig);
+
+
+    /** MINI_USARTs configured as follow:
+    - BaudRate = 115200 baud
+    - Word Length = 8 Bits
+    - One Stop Bit
+    - No parity
+    - Hardware flow control disabled (RTS and CTS signals)
+    - Receive and transmit enabled
+    */
+    /*
+
+    */
+    usartConfigStruct.baudRate = 115200;
+    usartConfigStruct.mode     = USART_MODE_TX_RX;
+    usartConfigStruct.hardwareFlowCtrl = USART_FLOW_CTRL_NONE;
+    usartConfigStruct.parity   = USART_PARITY_NONE;
+    usartConfigStruct.stopBits =  USART_STOP_BIT_1;
+    usartConfigStruct.wordLength = USART_WORD_LEN_8B;
+    USART_Config(USART1, &usartConfigStruct);
+
+    /** Enable USART_Interrupt_RXBNEIE */
+//    USART_EnableInterrupt(USART1, USART_INT_RXBNEIE);
+
+//    NVIC_EnableIRQRequest(USART1_IRQn, 2);
+   
+    /** Enable USART */
+    USART_Enable(USART1);
+}
+
